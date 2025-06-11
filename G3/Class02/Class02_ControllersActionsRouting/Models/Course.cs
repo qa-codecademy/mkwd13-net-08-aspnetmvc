ed99f164c1f76fa13932638cd78606ebb057a719
Hristijan Taseski <hristijan.taseski@gmail.com>
2025-06-11 22:39:01 +0200

    class02 Controllers Actions and Routing

---
+++ G3/Class02/Class02_ControllersActionsRouting/Models/Course.cs
@@ -0,0 +1,7 @@
+﻿namespace Class02_ControllersActionsRouting.Models {
+	public class Course {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int NumberOfClasses { get; set; }
+	}
+}
