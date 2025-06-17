d801fdcaafe8c067aeba21daa14585080bb67799
Danilo Borozan <daniloborozan07@gmail.com>
2025-06-17 19:51:23 +0200

    test

---
+++ G2/Class03/Qinshift.Class03.DemoApp/Class03/Models/DtoModels/StudentWithCourseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Class03.Models.DtoModels
+{
+    public class StudentWithCourseDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string NameOfCourse { get; set; }    
+    }
+}
---
+++ G2/Class03/Qinshift.Class03.DemoApp/Class03/Models/ViewModels/CreateCourseVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Class03.Models.ViewModels
+{
+    public class CreateCourseVM
+    {
+        public string Name { get; set; }
+        public int NoClasses { get; set; }
+    }
+}
