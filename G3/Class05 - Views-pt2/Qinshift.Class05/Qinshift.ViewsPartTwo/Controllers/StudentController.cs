48b00c2e5273733bca4d51d4e6664b7abc8f7723
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-06-18 18:37:03 +0200

    Class 05

---
+++ G3/Class05 - Views-pt2/Qinshift.Class05/Qinshift.ViewsPartTwo/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qinshift.ViewsPartTwo.Database;
+using Qinshift.ViewsPartTwo.Models.ViewModels;
+
+namespace Qinshift.ViewsPartTwo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<StudentViewModel> students = InMemoryDb.Students.Select(s => new StudentViewModel
+            {
+                Id = s.Id,
+                FullName = s.GetFullName(),
+                Age = DateTime.UtcNow.Year - s.DateOfBirth.Year,
+                ActiveCourseName = s.ActiveCourse?.Name ?? "No Course"
+            }).ToList();
+
+            return View(students);
+        }
+    }
+}
