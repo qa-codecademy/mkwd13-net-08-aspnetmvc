426677fa0f4becb47fb49eb5eafb6f0ced0fb8f7
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-06-25 17:33:48 +0200

    Class 07

---
+++ G3/Class07 - Model Binding/Qinshift.Class07/Qinshift.ModelBindingAndValidations/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qinshift.ModelBindingAndValidations.Database;
+using Qinshift.ModelBindingAndValidations.Models.Domain;
+using Qinshift.ModelBindingAndValidations.Models.ViewModels;
+
+namespace Qinshift.ModelBindingAndValidations.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<StudentViewModel> students = InMemoryDb.Students.Select(s => new StudentViewModel
+            {
+                Id = s.Id,
+                FullName = s.GetFullName(),
+                Age = DateTime.Now.Year - s.DateOfBirth.Year,
+                Email = s.Email
+            }).ToList();
+
+            ViewData["CurrentDate"] = DateTime.Now.ToShortDateString();
+            ViewBag.WelcomeMessage = "Welcome to the Student Management System";
+
+            return View(students);
+        }
+
+        // /student/create
+        //[HttpGet("student/add")]
+        public IActionResult Create()
+        {
+            CreateStudentViewModel createStudentViewModel = new();
+            return View(createStudentViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateStudentViewModel model)
+        {
+            var student = new Student
+            {
+                Id = InMemoryDb.Students.Count + 1,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                DateOfBirth = model.DateOfBirth,
+                PhoneNumber = model.PhoneNumber
+            };
+            InMemoryDb.Students.Add(student);
+            TempData["FormMessage"] = "Student succesfully created!";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
