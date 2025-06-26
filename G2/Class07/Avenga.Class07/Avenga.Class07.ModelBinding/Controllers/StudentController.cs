0cfb4ff27bb3ea97a078be80b0138173a703c35c
Martin <panovski.martin93@gmail.com>
2025-06-26 18:28:11 +0200

    Class 07 demo code part 1 added

---
+++ G2/Class07/Avenga.Class07/Avenga.Class07.ModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Avenga.Class07.ModelBinding.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avenga.Class07.ModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<StudentDto> students = new()
+            {
+                new StudentDto
+                {
+                    FullName = "Bob Bobsky",
+                    Age = 33
+                },
+                new StudentDto
+                {
+                    FullName = "Jill Wayne",
+                    Age = 20
+                },
+                new StudentDto
+                {
+                    FullName = "John Doe",
+                    Age = 40
+                },
+                new StudentDto
+                {
+                    FullName = "Martin Panovski",
+                    Age = 31
+                },
+            };
+            return View(students);
+        }
+    }
+}
