49982834aa6cb3ed1afad340f402b001e43407bb
Martin <panovski.martin93@gmail.com>
2025-06-26 20:50:08 +0200

    Class 07 demo code part 2 added

--- G2/Class07/Avenga.Class07/Avenga.Class07.ModelBinding/Models/Dtos/StudentDto.cs
+++ G2/Class07/Avenga.Class07/Avenga.Class07.ModelBinding/Models/Dtos/StudentDto.cs
@@ -4,9 +4,15 @@ namespace Avenga.Class07.ModelBinding.Models.Dtos
 {
     public class StudentDto
     {
+        public int Id { get; set; }
+
+
+        //[Display(Name = "Име на студент")]
         [Display(Name = "Student's full name")]
         public string FullName { get; set; }
 
+
+        //[Display(Name="Години")]
         [Display(Name = "Student's age")]
         public int Age { get; set; }
     }
