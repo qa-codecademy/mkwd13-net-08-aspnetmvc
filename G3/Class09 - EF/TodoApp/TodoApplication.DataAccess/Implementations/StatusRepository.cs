3dba9abf394fa0320535f2bcbc4f56969315cf85
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-06-30 19:47:39 +0200

    Class 09

---
+++ G3/Class09 - EF/TodoApp/TodoApplication.DataAccess/Implementations/StatusRepository.cs
@@ -0,0 +1,33 @@
+﻿using TodoApplication.DataAccess.Interfaces;
+using TodoApplication.Domain;
+
+namespace TodoApplication.DataAccess.Implementations {
+	public class StatusRepository : IRepository<Status> {
+		public void Add(Status entity) {
+			entity.Id = InMemoryDataBase.Statuses.Count + 1;
+			InMemoryDataBase.Statuses.Add(entity);
+		}
+
+		public void Delete(int id) {
+			var status = GetById(id);
+			if (status is not null) {
+				InMemoryDataBase.Statuses.Remove(status);
+			}
+		}
+		public IEnumerable<Status> GetAll() {
+			return InMemoryDataBase.Statuses;
+		}
+
+		public Status GetById(int id) {
+			return InMemoryDataBase.Statuses.FirstOrDefault(s => s.Id == id);
+		}
+
+		public void Update(Status entity) {
+			var status = GetById(entity.Id);
+			if (status is not null) {
+				var statusIndex = InMemoryDataBase.Statuses.IndexOf(status);
+				InMemoryDataBase.Statuses[statusIndex] = entity;
+			}
+		}
+	}
+}
---
+++ G3/Class09 - EF/TodoApp/TodoApplication.DataAccess/Implementations/TodoRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApplication.DataAccess.Interfaces;
+using TodoApplication.Domain;
+
+namespace TodoApplication.DataAccess.Implementations {
+	public class TodoRepository : ITodoRepository {
+		public void Add(Todo entity) {
+			entity.Id = InMemoryDataBase.Todos.Count + 1;
+			InMemoryDataBase.Todos.Add(entity);
+		}
+
+		public void Delete(int id) {
+			var todo = GetById(id);
+			if (todo is not null) 
+			{
+				InMemoryDataBase.Todos.Remove(todo);
+			}
+		}
+
+		public IEnumerable<Todo> GetAll() {
+			return InMemoryDataBase.Todos;
+		}
+
+		public Todo GetById(int id) {
+			return InMemoryDataBase.Todos.FirstOrDefault(t => t.Id == id);
+		}
+
+		public IEnumerable<Todo> GetCompletedTodos() {
+			return InMemoryDataBase.Todos.Where(t => t.StatusId == 2).ToList();
+		}
+
+		public void Update(Todo entity) {
+			var todo = GetById(entity.Id);
+			if(todo is not null) 
+			{
+				var todoIndex = InMemoryDataBase.Todos.IndexOf(todo);
+				InMemoryDataBase.Todos[todoIndex] = entity;
+			}
+		}
+	}
+}
